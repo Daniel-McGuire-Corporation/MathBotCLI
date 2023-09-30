7252d7a2d18ceb853a53ff1d4b96a93881c8982e
DanielLMcGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2023-09-30 18:47:25 -0500

    v3.0.0-beta.1
    Stable Release, Preparing For Something HUGE!

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // Copyright 2021 - 2024 Daniel McGuire Corporation
 //              All Rights Reserved.
 // Compiled Using dotnet on September 30th, 2023
-// **Above Info May Be Incorrect If Downloaded From GitHub Repo**
+// **Above Info May Be Incorrect If Cloned From GitHub Repo**
 using System;
 
 class Program
@@ -23,51 +23,63 @@ class Program
         return true;
     }
 
-   static void Main(string[] args)
-{
-    if (args.Length > 0)
+    static void Main(string[] args)
     {
-        switch(args[0])
+        if (args.Length > 0)
         {
-            case "--credits":
-            case "-c":
-                Console.Clear();
-                Console.WriteLine("Daniel McGuire Corporation Tools (R)");
-                Console.WriteLine();
-                Console.WriteLine("This Application was Written in C# by Daniel McGuire");
-                Console.WriteLine("(With Help From GitHub Copilot & ChatGPT)");
-                Console.WriteLine("Copyright (C) 2024 Daniel McGuire Corporation");
-                Console.WriteLine("This software is under GNU GP v3 License");
-                Console.WriteLine("Please visit https://www.daniellmcguirecorporation.ml for more info");
-                break;
-            case "--help":
-            case "-h":
-                Console.Clear();
-                Console.WriteLine("Daniel McGuire Corporation Tools (R)");
-                Console.WriteLine("Prime Number Solver Version 1.0");
-                Console.WriteLine();
-                Console.WriteLine("Usage:");
-                Console.WriteLine("  --credits, -c: Show credits");
-                Console.WriteLine("  --help, -h: Show help");
-                Console.WriteLine("  number: Check if a number is a prime number");
-                Console.WriteLine();
-                Console.WriteLine("Example: ./IsPrimeWrapper.exe <Number> <Arguments>");
-                break;
-            default:
-                int number = Convert.ToInt32(args[0]);
-                
-                if(IsPrime(number))
-                    Console.WriteLine(number + " is a prime number");
-                else
-                    Console.WriteLine(number + " is not a prime number");
-                break;
+            switch(args[0])
+            {
+                case "--credits":
+                case "-c":
+                    Console.Clear();
+                    Console.WriteLine("Daniel McGuire Corporation Tools (R)");
+                    Console.WriteLine();
+                    Console.WriteLine("This Application was Written in C# by Daniel McGuire");
+                    Console.WriteLine("(With Help From GitHub Copilot & ChatGPT)");
+                    Console.WriteLine("Copyright (C) 2024 Daniel McGuire Corporation");
+                    Console.WriteLine("This software is under GNU GP v3 License");
+                    Console.WriteLine("Please visit https://www.daniellmcguirecorporation.ml for more info");
+                    break;
+                case "--help":
+                case "-h":
+                    Console.Clear();
+                    Console.WriteLine("Daniel McGuire Corporation Tools (R)");
+                    Console.WriteLine("Prime Number Solver Version 3.0.0-beta.1");
+                    Console.WriteLine();
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("  --credits, -c: Show credits");
+                    Console.WriteLine("  --help, -h: Show help");
+                    Console.WriteLine("  --isprime <number>: Check if a number is a prime number");
+                    Console.WriteLine();
+                    Console.WriteLine("Example: ./IsPrimeWrapper.exe --isprime <Number>");
+                    break;
+                case "--isprime":
+                    if (args.Length > 1)
+                    {
+                        int number = Convert.ToInt32(args[1]);
+                        if(IsPrime(number))
+                            Console.WriteLine(number + " is a prime number.");
+                        else
+                            Console.WriteLine(number + " is not a prime number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please provide a number after --isprime");
+                    }
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Error: No Arguments Specified.");
+                    Console.WriteLine("Please specify an argument. Use --help for more information.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Error: No Numbers or Arguments Specified.");
+            Console.WriteLine("Please specify an argument. Use --help for more information.");
         }
     }
-    else
-    {
-        Console.Clear();
-        Console.WriteLine("Error: No Numbers or Arguments Specified.");
-        Console.WriteLine("Please specify an argument. Use --help for more information.");
-    }
 }
-}
+
