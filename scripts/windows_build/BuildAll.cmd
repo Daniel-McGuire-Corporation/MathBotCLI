e467606f460ca3978cedd641f46805580a7472f7
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-05-12 04:35:20 -0500

    Update

--- scripts/windows_build/BuildAll.cmd
+++ scripts/windows_build/BuildAll.cmd
@@ -2,12 +2,17 @@
 cd %~dp0
 set App=MathBotCLI
 title %App% Compiler 
-echo DMC C# Universal Compiling Script for %App%
+echo DMC C# Non-Universal Compiling Script for %App%
 echo.
 echo Compiling %App%
 echo.
 cd ..\..\src\Windows\
 dotnet build MathBotCLI.csproj --nologo -c Release -o bin\Release\net8.0-windows10.0.17763.0
+cd ..\Windows-NSIS-PathVari\PathEd\
+echo.
+echo Compiling Installer Path Component
+echo.
+dotnet build PathEd.csproj --nologo -c Release -o ..\..\Windows\bin\Release\net8.0-windows10.0.17763.0\Path\
 echo.
 echo Application Compiled!
 echo.
